f71912076abb7ee0e7abc180ea97f213f9f2434a
sharavanakkumar <sharavanakkumar@gmail.com>
2020-05-01 19:58:26 +0530

    Commit Service and Repository Interface, course view models

---
+++ CleanArch/CleanArch.Applicaation/Interfaces/ICourseService.cs
@@ -0,0 +1,12 @@
+﻿using CleanArch.Applicaation.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Applicaation.Interfaces
+{
+    public interface ICourseService
+    {
+        IEnumerable<CourseViewModel> GetCourses();
+    }
+}
