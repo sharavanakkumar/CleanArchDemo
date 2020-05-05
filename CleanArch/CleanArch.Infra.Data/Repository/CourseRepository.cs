db4de2c36d1e22f629ec6cc8eb5b7c33264f0a4b
sharavanakkumar <sharavanakkumar@gmail.com>
2020-05-05 15:53:39 +0530

    commit Dependency contaier

--- CleanArch/CleanArch.Infra.Data/Repository/CourseRepository.cs
+++ CleanArch/CleanArch.Infra.Data/Repository/CourseRepository.cs
@@ -16,6 +16,12 @@ namespace CleanArch.Infra.Data.Repository
             _ctx = ctx;
         }
 
+        public void Add(Course course)
+        {
+            _ctx.courses.Add(course);
+            _ctx.SaveChanges();
+        }
+
         public IEnumerable<Course> GetCourses()
         {
             return _ctx.courses;
--- CleanArch/CleanArch.Infra.IoC/DependencyContainer.cs
+++ CleanArch/CleanArch.Infra.IoC/DependencyContainer.cs
@@ -1,7 +1,12 @@
 ﻿using CleanArch.Applicaation.Interfaces;
 using CleanArch.Applicaation.Services;
+using CleanArch.Domain.Commands;
+using CleanArch.Domain.Core.Bus;
 using CleanArch.Domain.Interfaces;
+using CleanArch.Infra.Bus;
+using CleanArch.Infra.Data.Context;
 using CleanArch.Infra.Data.Repository;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -13,11 +18,19 @@ namespace CleanArch.Infra.IoC
     {
         public static void RegisterServices(IServiceCollection services)
         {
+            //Domain InMemory MediatR BUS
+            services.AddScoped<IMediatorHandler, InMemoryBus>();
+
+            //Domain Interface
+            services.AddScoped<IRequestHandler<CreateCourseCommand, bool>>();
+
             //Applicaion Layer
             services.AddScoped<ICourseService, CourseService>();
 
             //Data Layer
             services.AddScoped<ICourseRepository, CourseRepository>();
+
+            services.AddScoped<MyDBContext>();
         }
     }
 }
