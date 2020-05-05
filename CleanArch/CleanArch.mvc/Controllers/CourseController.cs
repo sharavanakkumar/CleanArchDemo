9e87f9227f9ab28c54cc93324f9ca14cf4fa6d06
sharavanakkumar <sharavanakkumar@gmail.com>
2020-05-05 10:55:00 +0530

    Inital commit for cqrs

---
+++ CleanArch/CleanArch.mvc/Controllers/CourseController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CleanArch.Applicaation.Interfaces;
+using CleanArch.Applicaation.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CleanArch.mvc.Controllers
+{
+    [Authorize]
+    public class CourseController : Controller
+    {
+        private ICourseService _courseService;
+
+        public CourseController(ICourseService courseservice)
+        {
+            _courseService = courseservice;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            CourseViewModel model = _courseService.GetCourses();
+            return View(model);
+        }
+    }
+}
